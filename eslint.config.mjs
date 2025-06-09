a4529fab48612c4ad4994932502df0c2680ff298
Louis Place <louis.place@epita.fr>
2025-06-09 17:46:00 +0200

    linter and formater setup

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,36 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import { dirname } from 'path'
+import { fileURLToPath } from 'url'
+import { FlatCompat } from '@eslint/eslintrc'
+import stylistic from '@stylistic/eslint-plugin'
+import btailwind from 'eslint-plugin-better-tailwindcss'
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
 
 const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
+	baseDirectory: __dirname,
+})
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-];
+	...compat.extends('next/core-web-vitals', 'next/typescript', 'prettier'),
+	{
+		files: ['**/*.ts', '**/*.tsx'],
+		plugins: {
+			'@stylistic': stylistic,
+			'better-tailwindcss': btailwind,
+		},
 
-export default eslintConfig;
+		languageOptions: {
+			parserOptions: {
+				project: './tsconfig.json',
+			},
+		},
+		rules: {
+			'@stylistic/semi': ['error', 'never'],
+			'@stylistic/quotes': ['error', 'single'],
+			'@typescript-eslint/no-unused-vars': 'error',
+		},
+	},
+]
+
+export default eslintConfig
