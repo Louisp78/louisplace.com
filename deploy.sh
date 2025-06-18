c1ac4964081cc24560f69fca77a4ee5e8958a0a0
Louis Place <louis.place@epita.fr>
2025-06-19 00:14:07 +0200

    secured CI/CD configuration

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #! /bin/sh
-cd /home/ubuntu/apps/louisplace.com || exit 1
-git pull origin main
-pnpm install
-pnpm build
-pm2 restart louisplace
+cd /var/www/louisplace.com || exit 1 &&
+git pull origin main &&
+pnpm install &&
+pnpm build &&
+sudo -u ubuntu /home/ubuntu/pm2-safe-restart.sh &&
 echo '✅ louisplace.com is well deployed !'
