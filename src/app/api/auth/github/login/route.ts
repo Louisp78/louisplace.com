e331ce67adb1a4dbe4b8777defa868b8605471e1
Louis Place <louis.place@epita.fr>
2025-12-15 19:38:54 +0100

    fix(auth): github specify auth response

--- src/app/api/auth/github/login/route.ts
+++ src/app/api/auth/github/login/route.ts
@@ -5,6 +5,7 @@ export async function GET() {
 	const params = new URLSearchParams({
 		client_id: Env.GITHUB_ID,
 		redirect_uri: `${Env.PUBLIC_URL}/api/auth/github/callback`,
+		response_type: 'code',
 		scope: 'read:user user:email',
 	})
 	return NextResponse.redirect(`https://github.com/login/oauth/authorize?${params}`)
