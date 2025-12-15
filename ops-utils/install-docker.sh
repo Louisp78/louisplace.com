b07c167bee12a24443e9ddd314c7f7d26ef7afd5
Louis Place <louis.place@epita.fr>
2025-12-16 00:06:45 +0100

    fix(ops): add group config to docker install

--- ops-utils/install-docker.sh
+++ ops-utils/install-docker.sh
@@ -16,3 +16,6 @@ EOF
 
 sudo apt update
 sudo apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+# add ubuntu user to docker group
+sudo usermod -aG docker ubuntu
