298e30804e2424eccaa87c131746f4caa525b3f0
Louis Place <louis.place@epita.fr>
2025-12-15 19:39:19 +0100

    feat(env): add google id to env

--- src/features/environment/env.ts
+++ src/features/environment/env.ts
@@ -4,6 +4,7 @@ const EnvSchema = z.object({
 	PUBLIC_BASE_URL: z.string('PUBLIC_BASE_URL must be a valid URL'),
 	PUBLIC_URL: z.url('PUBLIC_URL must be a valid URL'),
 	GITHUB_ID: z.string().min(1, 'GITHUB_ID is required'),
+	GOOGLE_ID: z.string().min(1, 'GOOGLE_ID is required'),
 })
 
 export type EnvironmentInterface = z.infer<typeof EnvSchema>
@@ -12,6 +13,7 @@ const Env: EnvironmentInterface = {
 	PUBLIC_BASE_URL: process.env.PUBLIC_BASE_URL!,
 	PUBLIC_URL: process.env.PUBLIC_URL!,
 	GITHUB_ID: process.env.GITHUB_ID!,
+	GOOGLE_ID: process.env.GOOGLE_ID!,
 }
 
 EnvSchema.parse(Env)
