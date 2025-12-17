da1e79c52c9a8f964309eb590504710212ec5f7f
Louis Place <louis.place@epita.fr>
2025-12-17 12:36:14 +0100

    fix(auth): await for params

--- client/src/app/api/auth/github/login/route.ts
+++ client/src/app/api/auth/github/login/route.ts
@@ -3,6 +3,6 @@ import { AuthProvider } from '@/features/auth/auth.service.interface'
 import { NextResponse } from 'next/server'
 
 export async function GET() {
-	const params = new AuthService().getParams(AuthProvider.GITHUB)
+	const params = await new AuthService().getParams(AuthProvider.GITHUB)
 	return NextResponse.redirect(`https://github.com/login/oauth/authorize?${params}`)
 }
--- client/src/app/api/auth/google/login/route.ts
+++ client/src/app/api/auth/google/login/route.ts
@@ -3,6 +3,6 @@ import { AuthProvider } from '@/features/auth/auth.service.interface'
 import { NextResponse } from 'next/server'
 
 export async function GET() {
-	const params = new AuthService().getParams(AuthProvider.GOOGLE)
+	const params = await new AuthService().getParams(AuthProvider.GOOGLE)
 	return NextResponse.redirect(`https://accounts.google.com/o/oauth2/v2/auth?${params}`)
 }
