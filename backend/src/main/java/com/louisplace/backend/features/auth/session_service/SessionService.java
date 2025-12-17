6b9485c63d08878ffdc0f6d3ca9c699b375b70fb
Louis Place <louis.place@epita.fr>
2025-12-17 18:57:38 +0100

    feat(auth): add session service

---
+++ backend/src/main/java/com/louisplace/backend/features/auth/session_service/SessionService.java
@@ -0,0 +1,24 @@
+package com.louisplace.backend.features.auth.session_service;
+
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+public class SessionService implements ISessionProvider {
+
+    @Override
+    public SecurityContext getSession(String email) {
+        UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(
+                email, // Principal (user identifier)
+                null, // Credentials (not needed after auth)
+                null // Authorities (roles/permissions)
+        );
+
+        // Set authentication in SecurityContext
+        SecurityContext securityContext = SecurityContextHolder.createEmptyContext();
+        securityContext.setAuthentication(authentication);
+        SecurityContextHolder.setContext(securityContext);
+        return securityContext;
+    }
+
+}
