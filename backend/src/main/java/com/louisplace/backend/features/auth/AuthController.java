3ab2dde2012bce4ae135f3844eff31198120018e
Louis Place <louis.place@epita.fr>
2025-12-17 18:58:00 +0100

    feat(auth): update controller to use new services

--- backend/src/main/java/com/louisplace/backend/features/auth/AuthController.java
+++ backend/src/main/java/com/louisplace/backend/features/auth/AuthController.java
@@ -1,6 +1,9 @@
 package com.louisplace.backend.features.auth;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.web.context.HttpSessionSecurityContextRepository;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,18 +11,34 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.louisplace.backend.features.auth.auth_service.AuthService;
+import com.louisplace.backend.features.auth.session_service.SessionService;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
 
 @RestController
 @RequestMapping("/auth")
 public class AuthController {
 
-    @Autowired
-    private AuthService authService;
+        @Autowired
+        private AuthService authService;
 
-    @PostMapping("/{provider}")
-    public String auth(@PathVariable String provider, @RequestParam String code) {
-        OAuthProvider oAuthProvider = OAuthProvider.fromString(provider);
-        authService.authenticate(oAuthProvider, code);
-        return "Authenticated with " + provider;
-    }
+        @Autowired
+        private SessionService sessionService;
+
+        @PostMapping("/auth/{provider}")
+        public ResponseEntity<UserDTO> auth(@PathVariable String provider, @RequestParam String code,
+                        HttpServletRequest request) {
+                UserEntity user = authService.authenticate("OAUTH", provider, code);
+
+                SecurityContext securityContext = sessionService.getSession(user.getEmail());
+
+                HttpSession session = request.getSession(true);
+                session.setAttribute(
+                                HttpSessionSecurityContextRepository.SPRING_SECURITY_CONTEXT_KEY,
+                                securityContext);
+
+                UserDTO userDTO = new UserDTO(user.getId(), user.getName(), user.getEmail());
+                return ResponseEntity.ok(userDTO);
+        }
 }
