7fe674555980a232396e790bc60e71dcdb708a9f
Louis Place <louis.place@epita.fr>
2025-12-17 17:47:09 +0100

    feat(user): add dto

---
+++ backend/src/main/java/com/louisplace/backend/features/auth/UserDTO.java
@@ -0,0 +1,12 @@
+package com.louisplace.backend.features.auth;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class UserDTO {
+    private Long id;
+    private String name;
+    private String email;
+}
