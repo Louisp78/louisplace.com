d50daf21f606d7ca29d1f3e444edfe72a1d83e94
Louis Place <louis.place@epita.fr>
2025-12-18 16:52:36 +0100

    chore(backend): complete security session

--- backend/src/main/java/com/louisplace/backend/config/SecurityConfig.java
+++ backend/src/main/java/com/louisplace/backend/config/SecurityConfig.java
@@ -4,13 +4,16 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
 @Configuration
 public class SecurityConfig {
         @Bean
         public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
                 http
-                                .csrf(csrf -> csrf.disable()) 
+                                .csrf(csrf -> csrf
+                                                .ignoringRequestMatchers("/auth/**")
+                                                .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse()))
                                 .cors(cors -> cors.configure(http))
                                 .authorizeHttpRequests(auth -> auth
                                                 .requestMatchers("/auth/**").permitAll()
