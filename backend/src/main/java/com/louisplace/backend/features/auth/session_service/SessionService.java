c62e27b0fd0029c3109a635aecdd2ed89b375612
Louis Place <louis.place@epita.fr>
2025-12-18 18:17:12 +0100

    refactor: delete unused roles

--- backend/src/main/java/com/louisplace/backend/features/auth/session_service/SessionService.java
+++ backend/src/main/java/com/louisplace/backend/features/auth/session_service/SessionService.java
@@ -1,26 +1,20 @@
 package com.louisplace.backend.features.auth.session_service;
 
-import java.util.List;
-
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
-
-import com.louisplace.backend.features.auth.Roles;
 
 @Service
 public class SessionService implements ISessionProvider {
 
     @Override
     public SecurityContext getSession(String email) {
-        List<SimpleGrantedAuthority> authorities = List.of(
-                new SimpleGrantedAuthority(Roles.ROLE_USER));
+
         UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(
                 email,
                 null,
-                authorities);
+                null);
 
         SecurityContext securityContext = SecurityContextHolder.createEmptyContext();
         securityContext.setAuthentication(authentication);
