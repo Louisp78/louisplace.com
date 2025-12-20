d9a2e093864801d6615bb7a5dd15bd0167c20d17
Louis Place <louis.place@epita.fr>
2025-12-20 17:32:49 +0100

    feat(link): setup icon of link component

--- client/src/features/post/components/post-link.tsx
+++ client/src/features/post/components/post-link.tsx
@@ -9,22 +9,28 @@ export default function PostLink({ href, children }: LinkProps) {
 	return (
 		<a
 			href={href}
-			className="inline-flex items-center gap-1 text-blue-500 hover:underline"
+			className="flex items-center gap-1 text-blue-500 hover:underline"
 			target="_blank"
 			rel="noopener noreferrer"
 		>
 			{children}
-			<svg
-				xmlns="http://www.w3.org/2000/svg"
-				width="16"
-				height="16"
-				fill="currentColor"
-				viewBox="0 0 16 16"
-				className="ml-1"
-			>
-				<path d="M6.354 5.5H2.5a.5.5 0 0 0 0 1h3.293l-4.147 4.146a.5.5 0 0 0 .708.708L6.5 7.207V10.5a.5.5 0 0 0 1 0V6.5a.5.5 0 0 0-.5-.5z" />
-				<path d="M10.5 2a.5.5 0 0 1 .5.5v11a.5.5 0 0 1-1 0v-11a.5.5 0 0 1 .5-.5z" />
-			</svg>
+			<div>
+				<svg
+					className="h-4 w-4"
+					data-testid="geist-icon"
+					viewBox="0 0 16 16"
+					preserveAspectRatio="xMidYMid meet"
+					strokeLinejoin="round"
+					aria-hidden="true"
+				>
+					<path
+						fillRule="evenodd"
+						clipRule="evenodd"
+						d="M5.75001 2H5.00001V3.5H5.75001H11.4393L2.21968 12.7197L1.68935 13.25L2.75001 14.3107L3.28034 13.7803L12.4988 4.56182V10.25V11H13.9988V10.25V3C13.9988 2.44772 13.5511 2 12.9988 2H5.75001Z"
+						fill="currentColor"
+					/>
+				</svg>
+			</div>
 		</a>
 	)
 }
