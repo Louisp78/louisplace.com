cd2eb7703fb60e834354c934358460f3364cb968
Louis Place <56790570+Louisp78@users.noreply.github.com>
2025-12-21 14:45:26 +0100

    fix(footer): use flex col to display footer elements (#13)

--- client/src/components/footer.tsx
+++ client/src/components/footer.tsx
@@ -5,23 +5,25 @@ import SPACING from '@/constants/spacing'
 export default function Footer() {
 	return (
 		<footer className="border-t-[0.5px] border-gray-300 p-5 text-center text-sm text-gray-500">
-			<span className="text-xs md:text-sm">{TEXT.footer.copyright}</span>
-			<span className="text-xs md:text-sm">{TEXT.footer.technologies}</span>
-			<a
-				className="inline-flex items-center gap-3 text-blue-700"
-				href={TEXT.github.url}
-				target="_blank"
-				rel="noopener noreferrer"
-			>
-				{TEXT.footer.visitGithub}
-				<Image
-					src="github.svg"
-					alt={TEXT.github.iconAlt}
-					width={SPACING.iconSize}
-					height={SPACING.iconSize}
-					className="inline"
-				/>
-			</a>
+			<div className="flex flex-col gap-1">
+				<span className="text-xs md:text-sm">{TEXT.footer.copyright}</span>
+				<span className="text-xs md:text-sm">{TEXT.footer.technologies}</span>
+				<a
+					className="flex justify-center gap-3 text-blue-700"
+					href={TEXT.github.url}
+					target="_blank"
+					rel="noopener noreferrer"
+				>
+					{TEXT.footer.visitGithub}
+					<Image
+						src="github.svg"
+						alt={TEXT.github.iconAlt}
+						width={SPACING.iconSize}
+						height={SPACING.iconSize}
+						className="inline"
+					/>
+				</a>
+			</div>
 		</footer>
 	)
 }
