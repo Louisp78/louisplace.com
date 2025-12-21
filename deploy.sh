dc42ce886bdb83913fd2ee3d6c9e880b56b0f347
Louis Place <louis.place@epita.fr>
2025-12-21 17:34:06 +0100

    chore: change the deploy to a relative cd cmd

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,6 @@
 #! /bin/sh
-cd louisplace.com || exit 1 &&
+SCRIPT_DIR="$(cd "$(dirname "$0")" >/dev/null 2>&1 && pwd)"
+cd "$SCRIPT_DIR" || exit 1 &&
 git pull origin main &&
 docker compose up -d --build &&
 echo '✅ louisplace.com is well deployed !'
