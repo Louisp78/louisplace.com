cad975121e5859e8123688ee80a72e9d7e214b45
Louis Place <louis.place@epita.fr>
2025-12-24 14:30:17 +0100

    test(user-entity-validator): use the public main method

--- backend/src/test/java/com/louisplace/backend/features/user/UserEntityValidatorTest.java
+++ backend/src/test/java/com/louisplace/backend/features/user/UserEntityValidatorTest.java
@@ -3,6 +3,8 @@ package com.louisplace.backend.features.user;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import com.louisplace.backend.features.user.validator.UserEntityValidator;
+
 public class UserEntityValidatorTest {
 
     UserEntityValidator validator;
@@ -15,8 +17,10 @@ public class UserEntityValidatorTest {
     @DisplayName("should email valid when classic format")
     void shouldHaveFieldsWellValidated() {
         String email = "example@example.com";
+        UserEntity user = new UserEntity();
+        user.setEmail(email);
 
-        boolean isValid = validator.validateEmail(email);
+        boolean isValid = validator.validateFields(user);
 
         assert (isValid);
     }
@@ -25,8 +29,10 @@ public class UserEntityValidatorTest {
     @DisplayName("should email invalid when missing at symbol")
     void shouldInvalidateEmailWhenMissingAtSymbol() {
         String email = "exampleexample.com";
+        UserEntity user = new UserEntity();
+        user.setEmail(email);
 
-        boolean isValid = validator.validateEmail(email);
+        boolean isValid = validator.validateFields(user);
 
         assert (!isValid);
     }
@@ -35,8 +41,10 @@ public class UserEntityValidatorTest {
     @DisplayName("should email invalid when missing domain")
     void shouldInvalidateEmailWhenMissingDomain() {
         String email = "example@.com";
+        UserEntity user = new UserEntity();
+        user.setEmail(email);
 
-        boolean isValid = validator.validateEmail(email);
+        boolean isValid = validator.validateFields(user);
 
         assert (!isValid);
     }
@@ -45,8 +53,10 @@ public class UserEntityValidatorTest {
     @DisplayName("should email invalid when missing extension")
     void shouldInvalidateEmailWhenMissingExtension() {
         String email = "example@example.";
+        UserEntity user = new UserEntity();
+        user.setEmail(email);
 
-        boolean isValid = validator.validateEmail(email);
+        boolean isValid = validator.validateFields(user);
 
         assert (!isValid);
     }
