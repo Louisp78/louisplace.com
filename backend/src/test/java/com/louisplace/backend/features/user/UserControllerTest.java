7673bbc08b1c97d98f6893e27ed43d3b4f73f8de
Louis Place <louis.place@epita.fr>
2025-12-24 10:33:20 +0100

    test(user): add tests for controller

--- backend/src/test/java/com/louisplace/backend/features/user/UserControllerTest.java
+++ backend/src/test/java/com/louisplace/backend/features/user/UserControllerTest.java
@@ -1,28 +1,61 @@
 package com.louisplace.backend.features.user;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.webclient.test.autoconfigure.AutoConfigureWebClient;
+import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
+@SpringBootTest
+@AutoConfigureMockMvc
+@AutoConfigureWebClient
 public class UserControllerTest {
 
-    @Test
-    @Disabled("TODO: should expose get current user profile endpoint")
-    void shouldExposeGetUserProfileEndpoint() {
+    UserController userController;
+
+    public UserControllerTest() {
+        this.userController = new UserController();
     }
 
     @Test
-    @Disabled("TODO: should get current user profile endpoint being protected according to user identity")
+    @DisplayName("should expose get current user profile endpoint")
+    void shouldExposeGetUserProfileEndpoint() {
+        ResponseEntity<UserDTO> response = userController.getUserInfos();
+
+        Assertions.assertEquals(200, response.getStatusCode().value());
+    }
+
+    @Test
+    @DisplayName("should get current user profile endpoint being protected according to user identity")
     void shouldGetUserProfileEndpointProtected() {
     }
 
     @Test
-    @Disabled("TODO: should expose update current user profile endpoint")
+    @DisplayName("should expose update current user profile endpoint")
     void shouldExposeUpdateUserProfileEndpoint() {
+        UserUpdateDTO updateUserDto = new UserUpdateDTO("First", "Last", "username", "email@example.com");
+
+        ResponseEntity<UserDTO> response = userController.updateUserInfos(updateUserDto);
+
+        Assertions.assertEquals(200, response.getStatusCode().value());
+        Assertions.assertNotNull(response.getBody());
     }
 
     @Test
-    @Disabled("TODO: should update current user profile endpoint being protected according to user identity")
-    void shouldUpdateUserProfileEndpointProtected() {
+    @DisplayName("should update current user profile endpoint being protected according to user identity")
+    void shouldUpdateUserProfileEndpointProtected(@Autowired MockMvc mvc) throws Exception {
+
+        // use mockmvc with security context to test protection
+        mvc.perform(MockMvcRequestBuilders.put("/users/me").contentType(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.status().isUnauthorized());
     }
 
 }
