c13a5e06bf0e939390d7a75eaf34119925c8ce36
Louis Place <louis.place@epita.fr>
2025-12-24 13:37:19 +0100

    test(auth): udpate post page tests to stay relative to authentification

--- client/src/tests/posts-page.test.tsx
+++
@@ -1,5 +0,0 @@
-describe('Posts Page', () => {
-	test.todo('should render a list of posts')
-	test.todo('should display user-info icon when user is logged in')
-	test.todo('should not display user-info icon when user is not logged in')
-})
