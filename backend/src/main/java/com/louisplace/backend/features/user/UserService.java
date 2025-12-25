dcc882a085458c43a4400f76843beeb0a9469085
Louis Place <louis.place@epita.fr>
2025-12-25 20:10:10 +0100

    feat(user): add field validation to service

--- backend/src/main/java/com/louisplace/backend/features/user/UserService.java
+++ backend/src/main/java/com/louisplace/backend/features/user/UserService.java
@@ -2,13 +2,17 @@ package com.louisplace.backend.features.user;
 
 import org.springframework.stereotype.Service;
 
+import com.louisplace.backend.features.user.validator.UserEntityValidator;
+
 @Service
 public class UserService implements IUserService {
 
     IUserRepository userRepository;
+    UserEntityValidator userEntityValidator;
 
-    public UserService(IUserRepository userRepository) {
+    public UserService(IUserRepository userRepository, UserEntityValidator userEntityValidator) {
         this.userRepository = userRepository;
+        this.userEntityValidator = userEntityValidator;
     }
 
     public UserEntity getUserInfo(String identifier) {
@@ -17,6 +21,14 @@ public class UserService implements IUserService {
     }
 
     public UserEntity updateUserInfo(UserUpdateDTO dataToUpdate, String identifier) {
+        UserEntity user = this.getUserInfo(identifier);
+        if (user == null) {
+            return null;
+        }
+
+        if (!userEntityValidator.validateFields(user)) {
+            return null;
+        }
         return new UserEntity();
     }
 
