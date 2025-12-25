9f1df3a59d428b977abf81fb36a38176469ade78
Louis Place <louis.place@epita.fr>
2025-12-25 22:38:52 +0100

    test(user): refactor mocking for controller

--- backend/src/test/java/com/louisplace/backend/features/user/UserControllerTest.java
+++ backend/src/test/java/com/louisplace/backend/features/user/UserControllerTest.java
@@ -1,8 +1,15 @@
 package com.louisplace.backend.features.user;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.lenient;
 import static org.mockito.Mockito.when;
 
-import org.junit.jupiter.api.Assertions;
+import java.util.Optional;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -28,47 +35,74 @@ public class UserControllerTest {
     @InjectMocks
     UserController userController;
 
-    @Test
-    @DisplayName("should expose get current user profile endpoint")
-    void shouldExposeGetUserProfileEndpoint() {
-        ResponseEntity<UserDTO> response = userController.getUserInfos();
+    static String email = "test@example.com";
+    static String firstName = "FirstName";
+    static String lastName = "LastName";
 
-        Assertions.assertEquals(HttpStatus.ACCEPTED, response.getStatusCode().value());
+    @BeforeEach
+    void beforeEach() {
+        lenient().when(sessionService.getPrincipal()).thenReturn(email);
+
+        UserEntity mockUser = new UserEntity();
+        mockUser.setEmail(email);
+        mockUser.setFirstName(firstName);
+        mockUser.setLastName(lastName);
+
+        lenient().when(userService.getUserInfo(email))
+                .thenReturn(java.util.Optional.of(mockUser));
+
+        lenient().when(userService.updateUserInfo(any(), any()))
+                .thenReturn(java.util.Optional.of(mockUser));
     }
 
     @Test
-    @DisplayName("should reject get current user profile when unauthenticated")
-    void shouldGetUserProfileEndpointProtected() {
-        when(sessionService.getPrincipal()).thenReturn(null);
+    @DisplayName("should expose get current user profile endpoint")
+    void shouldExposeGetUserProfileEndpoint() {
 
         ResponseEntity<UserDTO> response = userController.getUserInfos();
 
-        Assertions.assertEquals(HttpStatus.UNAUTHORIZED, response.getStatusCode().value());
+        assertEquals(HttpStatus.OK.value(), response.getStatusCode().value());
+        assertNotNull(response.getBody());
+    }
+
+    @Test
+    @DisplayName("should return not found when getting non existent user")
+    void shouldReturnNotFoundWhenGettingNonExistentUser() {
+        when(userService.getUserInfo(any()))
+                .thenReturn(Optional.empty());
+
+        ResponseEntity<UserDTO> response = userController.getUserInfos();
+        assertEquals(HttpStatus.NOT_FOUND.value(), response.getStatusCode().value());
+        assertNull(response.getBody());
     }
 
     @Test
     @DisplayName("should update current user profile given valid data for authenticated user")
-    void shouldExposeUpdateUserProfileEndpoint() {
-        UserUpdateDTO updateUserDto = new UserUpdateDTO("First", "Last");
 
-        when(sessionService.getPrincipal()).thenReturn("email@example.com");
+    void shouldExposeUpdateUserProfileEndpoint() {
+        String firstName = "FirstNameUpdated";
+        String lastName = "LastNameUpdated";
+        UserUpdateDTO updateUserDto = new UserUpdateDTO(Optional.of(firstName), Optional.of(lastName));
 
         ResponseEntity<UserDTO> response = userController.updateUserInfos(updateUserDto);
 
-        Assertions.assertEquals(HttpStatus.ACCEPTED, response.getStatusCode().value());
-        Assertions.assertNotNull(response.getBody());
+        assertEquals(HttpStatus.OK.value(), response.getStatusCode().value());
+        assertNotNull(response.getBody());
     }
 
     @Test
-    @DisplayName("should reject update current user profile endpoint for unauthenticated user")
-    void shouldUpdateUserProfileEndpointProtected() {
-        UserUpdateDTO updateUserDto = new UserUpdateDTO("First", "Last");
+    @DisplayName("should return not found when updating non existent user")
+    void shouldReturnNotFoundWhenUpdatingNonExistentUser() {
+        String firstName = "FirstNameUpdated";
+        String lastName = "LastNameUpdated";
+        UserUpdateDTO updateUserDto = new UserUpdateDTO(Optional.of(firstName), Optional.of(lastName));
 
-        when(sessionService.getPrincipal()).thenReturn(null);
+        when(userService.updateUserInfo(any(), any()))
+                .thenReturn(Optional.empty());
 
         ResponseEntity<UserDTO> response = userController.updateUserInfos(updateUserDto);
 
-        Assertions.assertEquals(HttpStatus.UNAUTHORIZED, response.getStatusCode().value());
+        assertEquals(HttpStatus.NOT_FOUND.value(), response.getStatusCode().value());
+        assertNull(response.getBody());
     }
-
 }
