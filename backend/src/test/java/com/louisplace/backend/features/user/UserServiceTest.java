cab3998befb34fff6626f180ea812019353024da
Louis Place <louis.place@epita.fr>
2025-12-25 22:38:28 +0100

    test(user): precise update user behavior not happy path and refactor mocking

--- backend/src/test/java/com/louisplace/backend/features/user/UserServiceTest.java
+++ backend/src/test/java/com/louisplace/backend/features/user/UserServiceTest.java
@@ -2,8 +2,13 @@ package com.louisplace.backend.features.user;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.mockito.Mockito.when;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.lenient;
 
+import java.util.Optional;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
@@ -12,6 +17,8 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import com.louisplace.backend.features.user.validator.UserEntityValidator;
+
 @ExtendWith(MockitoExtension.class)
 @DisplayName("UserService Tests")
 public class UserServiceTest {
@@ -19,8 +26,31 @@ public class UserServiceTest {
     @Mock
     private IUserRepository userRepository;
 
+    @Mock
+    private UserEntityValidator userEntityValidator;
+
     @InjectMocks
     private UserService userService;
+
+    static String email = "test@example.com";
+    static String firstName = "FirstName";
+    static String lastName = "LastName";
+
+    @BeforeEach
+    void beforeEach() {
+        lenient().when(userEntityValidator.validateFields(any())).thenReturn(true);
+
+        UserEntity mockUser = new UserEntity();
+        mockUser.setEmail(email);
+        mockUser.setFirstName(firstName);
+        mockUser.setLastName(lastName);
+
+        lenient().when(userRepository.findByEmail(email))
+                .thenReturn(java.util.Optional.of(mockUser));
+
+        lenient().when(userRepository.save(any(UserEntity.class)))
+                .thenAnswer(invocation -> invocation.getArgument(0));
+    }
 
     @Nested
     @DisplayName("UserService Tests")
@@ -29,29 +59,49 @@ public class UserServiceTest {
         @Test
         @DisplayName("Should successfully retrieve user by email")
         void shouldRetrieveUserByEmail() {
-            // Given
-            String email = "test@example.com";
-            String firstName = "FirstName";
-            String lastName = "LastName";
-            String username = "username";
-            UserEntity mockUser = new UserEntity();
-            mockUser.setEmail(email);
-            mockUser.setFirstName(firstName);
-            mockUser.setLastName(lastName);
-            mockUser.setUsername(username);
+            UserEntity user = userService.getUserInfo(email).orElse(null);
 
-            when(userRepository.findByEmail(email))
-                    .thenReturn(java.util.Optional.of(mockUser));
-
-            // When
-            UserEntity user = userService.getUserInfo(email);
-
-            // Then
             assertNotNull(user);
             assertEquals(email, user.getEmail());
             assertEquals(firstName, user.getFirstName());
             assertEquals(lastName, user.getLastName());
-            assertEquals(username, user.getUsername());
+        }
+
+        @Test
+        @DisplayName("Should return null when user not found by email")
+        void shouldReturnNullWhenUserNotFoundByEmail() {
+            UserEntity user = userService.getUserInfo("notfound@gmail.com").orElse(null);
+
+            assertNull(user);
+        }
+
+        @Test
+        @DisplayName("Should successfully update user information when user already exists")
+        void shouldUpdateUserInfo() {
+            String firstName = "UpdatedFirstName";
+            String lastName = "UpdatedLastName";
+
+            UserUpdateDTO updateDTO = new UserUpdateDTO(Optional.of(firstName), Optional.of(lastName));
+
+            UserEntity updatedUser = userService.updateUserInfo(email, updateDTO).orElse(null);
+
+            assertNotNull(updatedUser);
+            assertEquals(firstName, updatedUser.getFirstName());
+            assertEquals(lastName, updatedUser.getLastName());
+        }
+
+        @Test
+        @DisplayName("Should return null when trying to update non-existent user")
+        void shouldReturnNullWhenUpdatingNonExistentUser() {
+            String firstName = "FirstName";
+            String lastName = "LastName";
+            String email = "notfound@gmail.com";
+
+            UserUpdateDTO updateDTO = new UserUpdateDTO(Optional.of(firstName), Optional.of(lastName));
+
+            UserEntity updatedUser = userService.updateUserInfo(email, updateDTO).orElse(null);
+
+            assertNull(updatedUser);
         }
     }
 }
